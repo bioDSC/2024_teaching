fc840aa57c068c0c658cf65f90d21a89daecb7af
“Martijn <martijn.wehrens@gmail.com>
2025-03-07 12:34:25 +0100

    Finished L12 with extra exercises

--- 2025-02-Python-intro-Gapminder/202503_Py-Gapminder-testingcode.py
+++ 2025-02-Python-intro-Gapminder/202503_Py-Gapminder-testingcode.py
@@ -100,3 +100,146 @@ list_withtop
 list_line = [70*x-1000 for x in range(20)]
 list_line
 
+################################################################################
+
+for idx in range(10):
+    print(idx)
+    
+idx
+
+################################################################################
+
+acronym = ''
+
+for color in ['red','green','blue']:
+    acronym = acronym + color[0].upper()
+    # print(color[0])
+
+print(acronym)
+
+################################################################################
+
+
+for number in range(10):
+    
+    # use a if the number is a multiple of 3, otherwise use b
+    
+    if (Number % 3) == 0:
+        message = message + a
+    else:
+        message = message + "b"
+        
+print(message)
+
+# (some straightforward typos)
+
+################################################################################
+
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+data_all = pd.read_csv('/Users/m.wehrens/Data_UVA/2024_teaching/2025-03-gapminder/data/gapminder_all.csv', index_col='country')
+data_all.plot(kind='scatter', x='gdpPercap_2007', y='lifeExp_2007',
+              s=data_all['pop_2007']/1e6)
+
+# cannot be done:
+plt.text(data_all.loc[:,'gdpPercap_2007'].values, data_all.loc[:, 'lifeExp_2007'].values, list(data_all.index))
+    # https://stackoverflow.com/questions/54744772/matplotlib-text-not-taking-array-values
+
+
+plt.show()
+plt.close('all')
+
+#plt.text(data_all.loc['United States','gdpPercap_2007'], data_all.loc['United States','lifeExp_2007'], 'United States')
+#plt.text(data_all.loc['Netherlands','gdpPercap_2007'], data_all.loc['Netherlands','lifeExp_2007'], 'Netherlands')
+
+################################################################################
+
+# We'll be trying to recreate the following plot:
+# https://www.biodsc.nl/workshop-materials/plot.html
+
+# Solution for plotting exercise
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Load Europe data
+data_europe = pd.read_csv('/Users/m.wehrens/Data_UVA/2024_teaching/2025-03-gapminder/data/gapminder_gdp_europe.csv', index_col='country')
+
+# Check out shape
+data_europe.shape
+len(data_europe)
+
+### Reshape the dataframe to long format 
+# transpose
+data_europe_tranposed = data_europe.T
+# add year as int
+data_europe_tranposed['Year'] = data_europe.columns.str.replace('gdpPercap_', '').astype(int)
+# actual reshape to long format
+data_europe_tranposed_melted = data_europe_tranposed.melt(id_vars='Year', var_name='Country', value_name='GDP')
+
+### Plotting
+
+# get all countries
+countries_europe = data_europe.index
+
+# Start figure
+fig, ax = plt.subplots(6,5, figsize=(10/2.54,10/2.54)) # 2.54 = cm to inch factor
+
+# Looping to create data
+counter=0
+# Go over panels
+for idx_i in range(6):
+    for idx_j in range(5):                
+
+        # counter = which panel we're at (updated at end)
+        # not necessary for current situation
+        if counter < len(countries_europe):
+
+            # Determine which part dataframe we need
+            current_selection = data_europe_tranposed_melted['Country']==countries_europe[counter]
+            
+            # Make the plot
+            # IMPORTANT ARGUMENT: ax=ax[idx_i,idx_j]
+            data_europe_tranposed_melted.loc[current_selection,: ].plot(
+                    x='Year', y='GDP', ax=ax[idx_i,idx_j])
+            
+            # Add title
+            ax[idx_i, idx_j].set_title(countries_europe[counter], fontsize=5)
+            
+            # remove all labels except title
+            ax[idx_i, idx_j].set_xlabel('')
+            ax[idx_i, idx_j].set_ylabel('')
+            ax[idx_i, idx_j].set_xticks([])
+            ax[idx_i, idx_j].set_yticks([])
+            # remove legend
+            ax[idx_i, idx_j].get_legend().remove()
+            
+        # Administration for which country we are at
+        counter += 1
+
+# Saving            
+plt.tight_layout()            
+# plt.show()
+# save to pdf and png
+fig.savefig('/Users/m.wehrens/Desktop/202503_example-advanced.pdf', dpi=300, bbox_inches='tight')
+fig.savefig('/Users/m.wehrens/Desktop/202503_example-advanced.png', dpi=300, bbox_inches='tight')
+plt.close('all')
+
+################################################################################
+
+fig, ax = plt.subplots(6,5, figsize=(10/2.54,10/2.54)) # 2.54 = cm to inch factor
+
+# Looping to create data
+counter=0
+# Go over panels
+for idx_i in range(6):
+    for idx_j in range(5):  
+
+        x=[1,2,3]
+        y=[1,2,3]
+        ax[idx_i, idx_j].plot(x,y)
+        
+plt.show()
+plt.close('all')
