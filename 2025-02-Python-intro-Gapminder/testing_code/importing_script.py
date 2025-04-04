3e8c2ec2438185ece8296dc19449d7440fc3ad59
“Martijn <martijn.wehrens@gmail.com>
2025-04-04 15:44:28 +0200

    exercise update, test re question

---
+++ 2025-02-Python-intro-Gapminder/testing_code/importing_script.py
@@ -0,0 +1,11 @@
+
+
+import sys
+sys.path.append('/Users/m.wehrens/Documents/git_repos/_UVA/2024_teaching/2025-02-Python-intro-Gapminder/testing_code/')
+
+import source.custom_lib as custom_lib
+custom_lib.test()
+
+import custom_lib2 as cl2
+cl2.test()
+
